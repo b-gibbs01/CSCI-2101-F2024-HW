de4e7c152e34d56aaed860f00dc887d9d2e6000e
b-gibbs01 <gibbon30@go.stockton.edu>
2024-10-15 17:37:08 -0400

    Update Adlibs.java

--- HW1/Adlibs.java
+++ HW1/Adlibs.java
@@ -5,7 +5,7 @@ package HW1;
 // Hint3: A sample story if you are lost can be as simple as "Hello! My name is ____, and I am ____, and my favorite  
 //        color is ___"
 public class Adlibs {
-    public  String story(String name, int age, String favoriteColor) {
-        return null;
+    public static String story(String name, int age, String favoriteColor) {
+        return "There once was a hero named "+name+" the "+favoriteColor+" who came riding from Whiterun to old Rorikstead... also the was "+age ;
     }
 }
